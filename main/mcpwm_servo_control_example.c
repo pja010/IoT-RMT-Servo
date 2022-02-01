a81eec110f3545f09e8043cbbfc4feab8fb5d332
sebas8sebas <sebastianascoli@gmail.com>
2022-02-01 17:43:59 -0500

    ir control for servo"

--- main/mcpwm_servo_control_example.c
+++ main/mcpwm_servo_control_example.c
@@ -9,6 +9,11 @@
 #include "freertos/task.h"
 #include "driver/ledc.h"
 #include "esp_err.h"
+#include <string.h>
+#include "sdkconfig.h"
+#include "esp_log.h"
+#include "driver/rmt.h"
+#include "ir_tools.h"
 
 #define LEDC_TIMER              LEDC_TIMER_0
 #define LEDC_MODE               LEDC_LOW_SPEED_MODE
@@ -19,6 +24,75 @@
 #define LEDC_DUTY_MAX               (950)
 #define LEDC_FREQUENCY          (50) // Frequency in Hertz.
 #define MAX_ANGLE (180)
+#define ANGLE_DELTA (10)
+
+static const char *TAG = "receiver";
+static rmt_channel_t example_rx_channel = RMT_CHANNEL_2;
+
+
+static int angle_to_duty(int angle){
+    return (int)(LEDC_DUTY_MIN + (float)angle / MAX_ANGLE * (LEDC_DUTY_MAX - LEDC_DUTY_MIN));
+}
+
+static void example_ir_rx_task()//void *arg)
+{
+
+    int angle = 0;
+    uint32_t addr = 0;
+    uint32_t cmd = 0;
+    size_t length = 0;
+    bool repeat = false;
+    RingbufHandle_t rb = NULL;
+    rmt_item32_t *items = NULL;
+
+    rmt_config_t rmt_rx_config = RMT_DEFAULT_CONFIG_RX(CONFIG_EXAMPLE_RMT_RX_GPIO, example_rx_channel);
+    rmt_config(&rmt_rx_config);
+    rmt_driver_install(example_rx_channel, 1000, 0);
+    ir_parser_config_t ir_parser_config = IR_PARSER_DEFAULT_CONFIG((ir_dev_t)example_rx_channel);
+    ir_parser_config.flags |= IR_TOOLS_FLAGS_PROTO_EXT; // Using extended IR protocols (both NEC and RC5 have extended version)
+    ir_parser_t *ir_parser = NULL;
+#if CONFIG_EXAMPLE_IR_PROTOCOL_NEC
+    ir_parser = ir_parser_rmt_new_nec(&ir_parser_config);
+#elif CONFIG_EXAMPLE_IR_PROTOCOL_RC5
+    ir_parser = ir_parser_rmt_new_rc5(&ir_parser_config);
+#endif
+
+    //get RMT RX ringbuffer
+    rmt_get_ringbuf_handle(example_rx_channel, &rb);
+    assert(rb != NULL);
+    // Start receive
+    rmt_rx_start(example_rx_channel, true);
+    while (1) {
+        items = (rmt_item32_t *) xRingbufferReceive(rb, &length, portMAX_DELAY);
+        if (items) {
+            length /= 4; // one RMT = 4 Bytes
+            if (ir_parser->input(ir_parser, items, length) == ESP_OK) {
+                if (ir_parser->get_scan_code(ir_parser, &addr, &cmd, &repeat) == ESP_OK) {
+                    ESP_LOGI(TAG, "Scan Code %s --- addr: 0x%04x cmd: 0x%04x", repeat ? "(repeat)" : "", addr, cmd);
+                    if (cmd == 0xeb14) { //up arrow
+                        ESP_LOGI(TAG, "up");
+                        if (angle + ANGLE_DELTA <= MAX_ANGLE) angle += ANGLE_DELTA;
+                        ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, angle_to_duty(angle));
+                        ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+                        vTaskDelay(10);
+                    } else if (cmd == 0xef10){ // down arrow
+                        ESP_LOGI(TAG, "down");
+                        if (angle - ANGLE_DELTA >= 0) angle -= ANGLE_DELTA;
+                        ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, angle_to_duty(angle));
+                        ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+                        vTaskDelay(10);
+                    }
+                }
+            }
+            //after parsing the data, return spaces to ringbuffer.
+            vRingbufferReturnItem(rb, (void *) items);
+        }
+    }
+    ir_parser->del(ir_parser);
+    rmt_driver_uninstall(example_rx_channel);
+    vTaskDelete(NULL);
+}
+
 
 static void example_ledc_init(void)
 {
@@ -46,10 +120,6 @@ static void example_ledc_init(void)
 }
 
 
-static int angle_to_duty(int angle){
-    return (int)(LEDC_DUTY_MIN + (float)angle / MAX_ANGLE * (LEDC_DUTY_MAX - LEDC_DUTY_MIN));
-}
-
 
 void app_main(void)
 {
@@ -57,16 +127,17 @@ void app_main(void)
     example_ledc_init();
     vTaskDelay(10);
 
-    while(1){
-        ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, angle_to_duty(0));
-        ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-        vTaskDelay(100);
-        ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, angle_to_duty(90)); //819
-        ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-        vTaskDelay(100);
-        ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, angle_to_duty(180)); //819
-        ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-        vTaskDelay(100);
-    }
+    // while(1){
+    //     ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, angle_to_duty(0));
+    //     ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+    //     vTaskDelay(100);
+    //     ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, angle_to_duty(90)); //819
+    //     ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+    //     vTaskDelay(100);
+    //     ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, angle_to_duty(180)); //819
+    //     ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+    //     vTaskDelay(100);
+    // }
+    example_ir_rx_task();
 
 }
